9f813ef3bbede83ebb293a30ded05d95a4b83f99
Jonas Wanke <contact@jonas-wanke.com>
2020-09-07 17:30:00 +0200

    feat(packages/parser): export public API

---
+++ packages/parser/lib/parser.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+
+import 'package:parser/src/parser/grammar.dart';
+
+import 'src/parser/ast/general.dart';
+
+export 'src/lexer/token.dart';
+export 'src/parser/ast/declarations.dart';
+export 'src/parser/ast/expressions/expression.dart';
+export 'src/parser/ast/expressions/operator.dart';
+export 'src/parser/ast/general.dart';
+export 'src/parser/ast/statements.dart';
+export 'src/parser/ast/types.dart';
+
+CandyFile parseCandyFile(File file) {
+  assert(file != null);
+  if (!file.existsSync()) {
+    throw Exception("File ${file.absolute.path} doesn't exist.");
+  }
+
+  return parseCandySource(file.readAsStringSync());
+}
+
+CandyFile parseCandySource(String sourceCode) {
+  final result = ParserGrammar.candyFile.parse(sourceCode);
+  // TODO(JonasWanke): Better error handling for parser exceptions.
+  return result.value;
+}
