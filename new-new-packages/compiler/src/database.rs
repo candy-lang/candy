9650d5e8f54eb6b642689526877a00614a2ea0f0
Jonas Wanke <contact@jonas-wanke.com>
2022-01-29 11:41:22 +0100

    fix(compiler): work around Salsa caching problem

--- new-new-packages/compiler/src/database.rs
+++ new-new-packages/compiler/src/database.rs
@@ -4,7 +4,7 @@ use crate::{
     compiler::{
         ast_to_hir::AstToHirStorage, cst_to_ast::CstToAstStorage, string_to_cst::StringToCstStorage,
     },
-    input::{GetInputQuery, InputReference, InputStorage, InputWatcher},
+    input::{GetOpenInputQuery, InputReference, InputStorage, InputWatcher},
     language_server::{
         folding_range::FoldingRangeDbStorage, semantic_tokens::SemanticTokenDbStorage,
     },
@@ -29,7 +29,9 @@ impl Database {
     pub fn did_open_input(&mut self, input_reference: &InputReference, content: String) {
         let current_value = self.open_inputs.insert(input_reference.clone(), content);
         assert!(current_value.is_none());
-        GetInputQuery.in_db_mut(self).invalidate(input_reference);
+        GetOpenInputQuery
+            .in_db_mut(self)
+            .invalidate(input_reference);
     }
     pub fn did_change_input<F>(&mut self, input_reference: &InputReference, mapper: F)
     where
@@ -39,17 +41,21 @@ impl Database {
             .entry(input_reference.to_owned())
             .and_modify(mapper)
             .or_insert_with(|| panic!("Received a change for an input that was not opened."));
-        GetInputQuery.in_db_mut(self).invalidate(input_reference);
+        GetOpenInputQuery
+            .in_db_mut(self)
+            .invalidate(input_reference);
     }
     pub fn did_close_input(&mut self, input_reference: &InputReference) {
         self.open_inputs
             .remove(input_reference)
             .expect("Input was closed without being opened.");
-        GetInputQuery.in_db_mut(self).invalidate(input_reference);
+        GetOpenInputQuery
+            .in_db_mut(self)
+            .invalidate(input_reference);
     }
 }
 impl InputWatcher for Database {
-    fn get_open_input(&self, input_reference: &InputReference) -> Option<String> {
+    fn get_open_input_raw(&self, input_reference: &InputReference) -> Option<String> {
         self.open_inputs.get(input_reference).cloned()
     }
 }
--- new-new-packages/compiler/src/input.rs
+++ new-new-packages/compiler/src/input.rs
@@ -5,13 +5,12 @@ use salsa::query_group;
 #[query_group(InputStorage)]
 pub trait Input: InputWatcher {
     fn get_input(&self, input_reference: InputReference) -> Option<Arc<String>>;
+    fn get_open_input(&self, input_reference: InputReference) -> Option<Arc<String>>;
 }
 
 fn get_input(db: &dyn Input, input_reference: InputReference) -> Option<Arc<String>> {
-    db.salsa_runtime()
-        .report_synthetic_read(salsa::Durability::LOW);
-    if let Some(content) = db.get_open_input(&input_reference) {
-        return Some(Arc::new(content));
+    if let Some(content) = db.get_open_input(input_reference.clone()) {
+        return Some(content);
     };
 
     match input_reference {
@@ -23,13 +22,102 @@ fn get_input(db: &dyn Input, input_reference: InputReference) -> Option<Arc<Stri
         InputReference::Untitled(_) => None,
     }
 }
+fn get_open_input(db: &dyn Input, input_reference: InputReference) -> Option<Arc<String>> {
+    // The following line of code shouldn't be neccessary, but it is.
+    //
+    // We call `GetOpenInputQuery.in_db_mut(self).invalidate(input_reference);`
+    // in `Database.did_open_input(…)`, `.did_change_input(…)`, and
+    // `.did_close_input(…)` which correctly forces Salsa to re-run this query
+    // function the next time this input is used. However, even though the
+    // return value changes, Salsa doesn't record an updated `changed_at` value
+    // in its internal `ActiveQuery` struct. `Runtime.report_untracked_read()`
+    // manually sets this to the current revision.
+    db.salsa_runtime().report_untracked_read();
+
+    let content = db.get_open_input_raw(&input_reference)?;
+    Some(Arc::new(content))
+}
 
 pub trait InputWatcher {
-    fn get_open_input(&self, input_reference: &InputReference) -> Option<String>;
+    fn get_open_input_raw(&self, input_reference: &InputReference) -> Option<String>;
 }
 
 #[derive(Debug, PartialEq, Eq, Clone, Hash)]
 pub enum InputReference {
     File(PathBuf),
     Untitled(String),
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::{
+        compiler::{
+            cst::{self, Cst, CstKind},
+            string_to_cst::StringToCst,
+        },
+        database::Database,
+    };
+
+    #[test]
+    fn on_demand_input_works() {
+        let mut db = Database::default();
+        let input_reference = InputReference::File(PathBuf::from("/foo.rs"));
+
+        db.did_open_input(&input_reference, "123".to_owned());
+        assert_eq!(
+            db.get_input(input_reference.clone())
+                .unwrap()
+                .as_ref()
+                .to_owned(),
+            "123"
+        );
+        assert_eq!(
+            db.cst(input_reference.clone()).unwrap().as_ref().to_owned(),
+            vec![Cst {
+                id: cst::Id(0),
+                kind: CstKind::LeadingWhitespace {
+                    value: "\n".to_owned(),
+                    child: Box::new(Cst {
+                        id: cst::Id(1),
+                        kind: CstKind::Int {
+                            offset: 0,
+                            value: 123,
+                            source: "123".to_owned(),
+                        }
+                    })
+                }
+            }],
+        );
+
+        db.did_change_input(&input_reference, |content| {
+            *content = "456".to_owned();
+        });
+        assert_eq!(
+            db.get_input(input_reference.clone())
+                .unwrap()
+                .as_ref()
+                .to_owned(),
+            "456"
+        );
+        assert_eq!(
+            db.cst(input_reference.clone()).unwrap().as_ref().to_owned(),
+            vec![Cst {
+                id: cst::Id(0),
+                kind: CstKind::LeadingWhitespace {
+                    value: "\n".to_owned(),
+                    child: Box::new(Cst {
+                        id: cst::Id(1),
+                        kind: CstKind::Int {
+                            offset: 0,
+                            value: 456,
+                            source: "456".to_owned(),
+                        }
+                    })
+                }
+            }],
+        );
+
+        db.did_close_input(&input_reference);
+    }
 }
