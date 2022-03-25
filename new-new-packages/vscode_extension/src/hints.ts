ee2303035fd3cff5f0eabe8b5951ccd8a34dad2a
Jonas Wanke <contact@jonas-wanke.com>
2022-03-25 10:04:50 +0100

    refactor(vscode_extension): make code cleaner

--- new-new-packages/vscode_extension/src/hints.ts
+++ new-new-packages/vscode_extension/src/hints.ts
@@ -1,5 +1,5 @@
 // Lots of this code are taken from Dart-Code:
-// https://github.com/Dart-Code/Dart-Code/blob/1d86cf3a4fcb3653376092f6677447cd9870b98e/src/extension/lsp/closing_hints_decorations.ts
+// https://github.com/Dart-Code/Dart-Code/blob/075f71ca0336e94ebb480be35895b5b12314223b/src/extension/lsp/closing_labels_decorations.ts
 import * as vs from 'vscode';
 import { LanguageClient } from 'vscode-languageclient/node';
 import {
@@ -10,51 +10,51 @@ import {
 
 export class HintsDecorations implements vs.Disposable {
   private subscriptions: vs.Disposable[] = [];
-  private hints: Map<String, Hint[]> = new Map();
-  private editors: Map<String, vs.TextEditor> = new Map();
+  private hints = new Map<String, Hint[]>();
+  private editors = new Map<String, vs.TextEditor>();
   private updateTimeout?: NodeJS.Timer;
 
-  private readonly decorationTypes: Map<HintKind, vs.TextEditorDecorationType> =
-    new Map([
-      [
-        'value',
-        vs.window.createTextEditorDecorationType({
-          after: { color: new vs.ThemeColor('candy.hints.valueColor') },
-          rangeBehavior: vs.DecorationRangeBehavior.ClosedOpen,
-        }),
-      ],
-      [
-        'panic',
-        vs.window.createTextEditorDecorationType({
-          after: { color: new vs.ThemeColor('candy.hints.panicColor') },
-          rangeBehavior: vs.DecorationRangeBehavior.ClosedOpen,
-        }),
-      ],
-    ]);
+  private readonly decorationTypes = new Map([
+    [
+      'value',
+      vs.window.createTextEditorDecorationType({
+        after: { color: new vs.ThemeColor('candy.hints.valueColor') },
+        rangeBehavior: vs.DecorationRangeBehavior.ClosedOpen,
+      }),
+    ],
+    [
+      'panic',
+      vs.window.createTextEditorDecorationType({
+        after: { color: new vs.ThemeColor('candy.hints.panicColor') },
+        rangeBehavior: vs.DecorationRangeBehavior.ClosedOpen,
+      }),
+    ],
+  ]);
 
   constructor(private readonly analyzer: LanguageClient) {
     // tslint:disable-next-line: no-floating-promises
     analyzer.onReady().then(() => {
-      this.analyzer.onNotification(PublishHintsNotification.type, (n) => {
-        console.log('Received hints!');
+      this.analyzer.onNotification(
+        PublishHintsNotification.type,
+        (notification) => {
+          // We parse the URI so that it gets normalized.
+          const uri = vs.Uri.parse(notification.uri).toString();
 
-        // We parse the URI so that it gets normalized.
-        const uri = vs.Uri.parse(n.uri).toString();
-
-        this.hints.set(uri, n.hints);
-        // Fire an update if it was for the active document.
-        if (
-          vs.window.activeTextEditor &&
-          vs.window.activeTextEditor.document &&
-          uri === vs.window.activeTextEditor.document.uri.toString()
-        ) {
-          // Delay this so if we're getting lots of updates, we don't flicker.
-          if (this.updateTimeout) {
-            clearTimeout(this.updateTimeout);
+          this.hints.set(uri, notification.hints);
+          // Fire an update if it was for the active document.
+          if (
+            vs.window.activeTextEditor &&
+            vs.window.activeTextEditor.document &&
+            uri === vs.window.activeTextEditor.document.uri.toString()
+          ) {
+            // Delay this so if we're getting lots of updates, we don't flicker.
+            if (this.updateTimeout) {
+              clearTimeout(this.updateTimeout);
+            }
+            this.updateTimeout = setTimeout(() => this.update(), 500);
           }
-          this.updateTimeout = setTimeout(() => this.update(), 500);
         }
-      });
+      );
     });
 
     this.subscriptions.push(
@@ -84,14 +84,14 @@ export class HintsDecorations implements vs.Disposable {
     type Item = vs.DecorationOptions & {
       renderOptions: { after: { contentText: string } };
     };
-    const decorations: Map<HintKind, Item[]> = new Map();
+    const decorations = new Map<HintKind, Item[]>();
     for (const hint of hints) {
       const position = this.analyzer.protocol2CodeConverter.asPosition(
         hint.position
       );
 
       // Ensure the hint we got looks like a sensible position, otherwise the type info
-      // might be stale (eg. we sent two updates, and the type from in between them just
+      // might be stale (e.g., we sent two updates, and the type from in between them just
       // arrived). In this case, we'll just bail and do nothing, assuming a future update will
       // have the correct info.
       // TODO(later, JonasWanke): do we really need this check?
