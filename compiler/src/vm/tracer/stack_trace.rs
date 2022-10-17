f2c848acf093d50120875b374e09145ab43856fe
Marcel Garus <github@marcelgarus.dev>
2022-10-17 21:22:44 +0200

    Remove logging all stack traces

--- compiler/src/vm/tracer/stack_trace.rs
+++ compiler/src/vm/tracer/stack_trace.rs
@@ -11,7 +11,6 @@ use crate::{
 };
 use itertools::Itertools;
 use std::collections::HashMap;
-use tracing::debug;
 
 // Stack traces are a reduced view of the tracing state that represent the stack
 // trace at a given moment in time.
@@ -160,7 +159,6 @@ impl FullTracer {
         }
 
         let stack_traces = self.stack_traces();
-        debug!("Stack traces: {:?}", stack_traces.keys().collect_vec());
         panicking_fiber_chain
             .into_iter()
             .rev()
