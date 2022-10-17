1b51a9435a0ad946e590efeee6aaa1d55d88e507
Marcel Garus <github@marcelgarus.dev>
2022-10-17 22:06:59 +0200

    Remove let_else feature flag

--- compiler/src/main.rs
+++ compiler/src/main.rs
@@ -2,7 +2,6 @@
 #![feature(box_patterns)]
 #![feature(generic_associated_types)]
 #![feature(let_chains)]
-#![feature(let_else)]
 #![feature(never_type)]
 #![feature(try_trait_v2)]
 #![allow(clippy::module_inception)]
