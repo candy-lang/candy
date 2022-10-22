d2bf96773559bd2361b423c43a87d6515976ac1c
Marcel Garus <github@marcelgarus.dev>
2022-10-22 22:45:33 +0200

    Don't tree-shake impure expressions

--- compiler/src/compiler/optimize/tree_shaking.rs
+++ compiler/src/compiler/optimize/tree_shaking.rs
@@ -1,47 +1,69 @@
-use crate::{
-    builtin_functions::BuiltinFunction,
-    compiler::hir::{Body, Expression, Id, Lambda},
-};
+use crate::compiler::hir::{Body, Expression, Lambda};
 use itertools::Itertools;
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 use tracing::warn;
 
 impl Body {
     pub fn tree_shake(&mut self) {
-        let mut still_needed = HashSet::new();
-        still_needed.insert(self.return_value());
+        let mut keep = HashSet::new();
+        keep.insert(self.return_value());
 
         // TODO: Do this without cloning.
         for id in self.ids.clone().into_iter().rev() {
             // warn!(
-            //     "Used: {}",
-            //     still_needed.iter().map(|id| format!("{id}")).join(", ")
+            //     "Tree shaking keep list: {}",
+            //     keep.iter().map(|id| format!("{id}")).join(", ")
             // );
 
-            if still_needed.contains(&id) {
+            if self.expressions.get(&id).unwrap().is_pure() != Some(true) {
+                keep.insert(id.clone());
+            }
+
+            if keep.contains(&id) {
                 let mut child_ids = vec![];
                 self.expressions
                     .get(&id)
                     .unwrap()
                     .collect_all_ids(&mut child_ids);
-                still_needed.extend(child_ids.into_iter());
+                if format!("{child_ids:?}").contains("✨") {
+                    warn!("Sparkles were added to the keep list because {id} needs them.");
+                }
+                keep.extend(child_ids.into_iter());
 
-                if let Expression::Lambda(Lambda { body, .. }) =
-                    self.expressions.get_mut(&id).unwrap()
-                {
-                    body.tree_shake();
+                if let Expression::Lambda(lambda) = self.expressions.get_mut(&id).unwrap() {
+                    lambda.body.tree_shake();
                 }
             } else {
                 warn!("{id} will be optmized away");
             }
         }
 
-        let used = self
-            .ids
-            .iter()
-            .filter(|id| still_needed.contains(id))
-            .cloned()
-            .collect_vec();
-        self.ids = used;
+        self.ids.retain(|id| keep.contains(id));
+        self.expressions.retain(|id, _| keep.contains(id));
+        self.identifiers.retain(|id, _| keep.contains(id));
+    }
+}
+
+impl Expression {
+    fn is_pure(&self) -> Option<bool> {
+        match self {
+            Expression::Int(_) => Some(true),
+            Expression::Text(_) => Some(true),
+            Expression::Reference(_) => Some(true),
+            Expression::Symbol(_) => Some(true),
+            Expression::Struct(_) => Some(true),
+            Expression::Lambda(_) => Some(true),
+            Expression::Builtin(_) => Some(true),
+            Expression::Call {
+                function,
+                arguments,
+            } => None,
+            Expression::UseModule {
+                current_module,
+                relative_path,
+            } => Some(false),
+            Expression::Needs { condition, reason } => Some(false),
+            Expression::Error { child, errors } => Some(false),
+        }
     }
 }
