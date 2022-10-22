bbd73f7106cd87761482c2eea3dfc2e066922bc5
Marcel Garus <github@marcelgarus.dev>
2022-10-22 22:44:26 +0200

    Resolve references referring to outer expressions

--- compiler/src/compiler/optimize/follow_references.rs
+++ compiler/src/compiler/optimize/follow_references.rs
@@ -1,14 +1,14 @@
-use crate::{
-    builtin_functions::BuiltinFunction,
-    compiler::hir::{Body, Expression, Id, Lambda},
-};
-use std::collections::HashMap;
+use crate::compiler::hir::{Body, Expression, Id};
+use im::HashMap;
 use tracing::warn;
 
 impl Body {
     pub fn follow_references(&mut self) {
-        let mut replacements = HashMap::<Id, Id>::new();
+        let replacements = HashMap::<Id, Id>::new();
+        self.follow_inner_references(replacements);
+    }
 
+    fn follow_inner_references(&mut self, mut replacements: HashMap<Id, Id>) {
         for id in self.ids.clone() {
             if let Expression::Reference(reference) = self.expressions.get(&id).unwrap() {
                 let resolved = replacements
@@ -20,11 +20,12 @@ impl Body {
                 let expression = self.expressions.get_mut(&id).unwrap();
                 expression.replace_ids(&mut |id| {
                     if let Some(replacement) = replacements.get(id) {
+                        warn!("Following reference");
                         *id = replacement.clone();
                     }
                 });
                 if let Expression::Lambda(lambda) = expression {
-                    lambda.body.follow_references();
+                    lambda.body.follow_inner_references(replacements.clone());
                 }
             }
         }
