46f01ba05764aaca9be42fa0bb9935a50bb948c3
Marcel Garus <marcel.garus@gmail.com>
2022-10-29 20:36:49 +0200

    Update compiler/src/vm/fiber.rs
    Co-authored-by: Jonas Wanke <contact@jonas-wanke.com>

--- compiler/src/vm/fiber.rs
+++ compiler/src/vm/fiber.rs
@@ -123,7 +123,7 @@ impl Fiber {
         assert_eq!(
             closure.captured.len(),
             0,
-            "closure is not a module closure (it captures stuff)."
+            "Closure is not a module closure (it captures stuff)."
         );
         assert_eq!(
             closure.num_args, 0,
