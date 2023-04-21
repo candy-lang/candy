fba27c98a495f4af3a9d4f89e63e043970337c34
Jonas Wanke <contact@jonas-wanke.com>
2023-04-21 10:49:03 +0200

    Remove superfluous lifetime argument

--- compiler/vm/src/utils.rs
+++ compiler/vm/src/utils.rs
@@ -13,12 +13,12 @@ pub trait DebugDisplay: Debug + Display {
 macro_rules! impl_debug_display_via_debugdisplay {
     ($type:ty) => {
         impl std::fmt::Debug for $type {
-            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
                 DebugDisplay::fmt(self, f, true)
             }
         }
         impl std::fmt::Display for $type {
-            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
                 DebugDisplay::fmt(self, f, false)
             }
         }
