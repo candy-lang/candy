13913a4d34d05925608ba2c1de00e420d567d5a5
Marcel Garus <github@marcelgarus.dev>
2023-05-05 19:21:36 +0200

    Apply suggestions

--- compiler/fuzzer/src/runner.rs
+++ compiler/fuzzer/src/runner.rs
@@ -2,7 +2,7 @@ use candy_frontend::hir::Id;
 use candy_vm::{
     self,
     context::{CombiningExecutionController, CountingExecutionController, ExecutionController},
-    heap::{Closure, HirId},
+    heap::{Closure, HirId, InlineObjectSliceCloneToHeap},
     lir::Lir,
     tracer::full::FullTracer,
     vm::{self, Vm},
@@ -52,13 +52,15 @@ impl RunResult {
 impl<L: Borrow<Lir>> Runner<L> {
     pub fn new(lir: L, closure: Closure, input: Input) -> Self {
         let (mut heap, constant_mapping) = lir.borrow().constant_heap.clone();
+        let closure = closure.clone_to_heap(&mut heap).try_into().unwrap();
+        let arguments = input.arguments.clone_to_heap(&mut heap);
         let responsible = HirId::create(&mut heap, Id::fuzzer());
         let vm = Vm::for_closure(
             lir,
             heap,
             constant_mapping,
             closure,
-            &input.arguments,
+            &arguments,
             responsible,
         );
 
