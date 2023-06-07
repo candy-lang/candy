15918d4001b773e7c57e228db7d20dee3cf5d7c6
Jonas Wanke <contact@jonas-wanke.com>
2023-06-07 11:46:26 +0200

    Support viewing IR when focused on another iR

--- vscode_extension/src/debug_irs.ts
+++ vscode_extension/src/debug_irs.ts
@@ -183,24 +183,37 @@ async function pickTracingMode(
 
 const irScheme = 'candy-ir';
 function encodeUri(document: vscode.TextDocument, ir: Ir): vscode.Uri {
+  let [uri, moduleKind] =
+    document.uri.scheme === irScheme
+      ? (() => {
+          const { originalUri, moduleKind } = decodeUri(document.uri);
+          return [originalUri, moduleKind];
+        })()
+      : [document.uri, getModuleKind(document.languageId)];
   const details: { [key: string]: any } = {
-    scheme: document.uri.scheme,
-    moduleKind: getModuleKind(document.languageId),
+    scheme: uri.scheme,
+    moduleKind,
     ...ir,
   };
   delete details.type;
 
   return vscode.Uri.from({
     scheme: irScheme,
-    path: `${document.uri.path}.${ir.type}`,
+    path: `${uri.path}.${ir.type}`,
     // TODO: Encode this in the query part once VS Code doesn't encode it again.
     fragment: JSON.stringify(details),
   });
 }
-function decodeUri(uri: vscode.Uri): { ir: Ir; originalUri: vscode.Uri } {
+function decodeUri(uri: vscode.Uri): {
+  ir: Ir;
+  originalUri: vscode.Uri;
+  moduleKind: ModuleKind;
+} {
   const details = JSON.parse(uri.fragment);
   const scheme = details.scheme as string;
   delete details.scheme;
+  const moduleKind = details.moduleKind as ModuleKind;
+  delete details.moduleKind;
 
   const separatorIndex = uri.path.lastIndexOf('.');
   const path = uri.path.slice(0, separatorIndex);
@@ -212,5 +225,6 @@ function decodeUri(uri: vscode.Uri): { ir: Ir; originalUri: vscode.Uri } {
   return {
     ir,
     originalUri: vscode.Uri.from({ scheme, path }),
+    moduleKind,
   };
 }
