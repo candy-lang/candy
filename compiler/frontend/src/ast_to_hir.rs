21483138617cc8b3f41b4a9575b6774a282ae101
Marcel Garus <github@marcelgarus.dev>
2023-08-06 18:13:15 +0200

    Don't propagate error children past the AST

--- compiler/frontend/src/ast_to_hir.rs
+++ compiler/frontend/src/ast_to_hir.rs
@@ -904,15 +904,10 @@ impl<'a> PatternContext<'a> {
                     .collect();
                 Pattern::Or(patterns)
             }
-            AstKind::Error { child, errors, .. } => {
-                let child = child
-                    .as_ref()
-                    .map(|child| Box::new(self.compile_pattern(child)));
-                Pattern::Error {
-                    child,
-                    errors: errors.to_owned(),
-                }
-            }
+            AstKind::Error { errors, .. } => Pattern::Error {
+                child: None,
+                errors: errors.to_owned(),
+            },
         }
     }
 
