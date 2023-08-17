609bd2857bd090a6107849e88c16cda91f1d9757
Jonas Wanke <contact@jonas-wanke.com>
2023-08-17 15:21:48 +0200

    Remove superfluous trailing commas

--- compiler/cli/src/run.rs
+++ compiler/cli/src/run.rs
@@ -135,7 +135,7 @@ pub(crate) fn run(options: Options) -> ProgramResult {
                 Err(panic) => {
                     error!("The main function panicked: {}", panic.reason);
                     error!("{} is responsible.", panic.responsible);
-                    error!("This is the stack trace:\n{}", tracer.format(&db),);
+                    error!("This is the stack trace:\n{}", tracer.format(&db));
                     break Err(Exit::CodePanicked);
                 }
             },
