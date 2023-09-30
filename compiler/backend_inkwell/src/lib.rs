fd34cf4b8c7f91c1d6c90b282a877d962946c242
Marcel Garus <github@marcelgarus.dev>
2023-09-30 12:58:56 +0200

    Limit int ranges to u64

--- compiler/backend_inkwell/src/lib.rs
+++ compiler/backend_inkwell/src/lib.rs
@@ -320,7 +320,7 @@ impl<'ctx> CodeGen<'ctx> {
                     let i64_type = self.context.i64_type();
                     let v = i64_type.const_int(
                         value
-                            .clamp(&i32::MIN.into(), &i32::MAX.into())
+                            .clamp(&u64::MIN.into(), &u64::MAX.into())
                             .try_into()
                             .unwrap(),
                         false,
