dd5f3671dd6377333555bb808c5ece701aec5ccb
Jonas Wanke <contact@jonas-wanke.com>
2024-02-22 15:32:32 +0100

    Simplify benchmark execution

--- compiler/vm/benches/benchmark.rs
+++ compiler/vm/benches/benchmark.rs
@@ -4,19 +4,16 @@
 use candy_frontend::module::PackagesPath;
 use candy_vm::{
     byte_code::ByteCode,
-    environment::Environment,
-    heap::{Data, Handle, Heap, InlineObject, Int, List, Struct, Tag, Text},
-    tracer::{stack_trace::StackTracer, Tracer},
-    Vm, VmFinished, VmHandleCall,
+    heap::{Heap, Struct},
+    tracer::stack_trace::StackTracer,
+    Vm, VmFinished,
 };
 use environment::BenchmarkingEnvironment;
-use iai_callgrind::{black_box, library_benchmark, library_benchmark_group, main};
-use itertools::Itertools;
-use std::{
-    borrow::Borrow,
-    fs, iter,
-    path::{self},
+use iai_callgrind::{
+    black_box, library_benchmark, library_benchmark_group, main, FlamegraphConfig,
+    LibraryBenchmarkConfig,
 };
+use std::fs;
 use tracing::Level;
 use tracing_subscriber::{
     filter,
@@ -25,51 +22,29 @@ use tracing_subscriber::{
     util::SubscriberInitExt,
     Layer,
 };
-use utils::{compile, setup, setup_and_compile, Database};
+use utils::{compile, setup, Database};
 
 mod environment;
 mod utils;
 
 #[library_benchmark]
-#[bench::main(prepare("Examples/helloWorld.candy", &[]))]
-fn compilation_hello_world(program: PreparedProgram) {
-    run_program(program);
+#[bench::examples_fibonacci(compile_prepare("Examples/fibonacci.candy"))]
+#[bench::examples_hello_world(compile_prepare("Examples/helloWorld.candy"))]
+fn compile((mut db, source_code): (Database, String)) {
+    utils::compile(&mut db, &source_code);
 }
-#[library_benchmark]
-#[bench::main(prepare("Examples/fibonacci.candy", &["15"]))]
-fn compilation_fibonacci(program: PreparedProgram) {
-    run_program(program);
-}
-
-struct PreparedProgram {
-    db: Database,
-    byte_code: ByteCode,
-    heap: Heap,
-    environment: BenchmarkingEnvironment,
-    environment_argument: Struct,
-}
-fn prepare(file_path: &str, arguments: &[&str]) -> PreparedProgram {
+fn compile_prepare(file_path: &str) -> (Database, String) {
     init_logger();
 
+    let db = setup();
     let source_code = fs::read_to_string(format!("../../packages/{file_path}")).unwrap();
-
-    // let byte_code = setup_and_compile(&source_code);
-    let mut db = setup();
-    let byte_code = compile(&mut db, &source_code);
-
-    let mut heap = Heap::default();
-    let (environment_argument, environment) = BenchmarkingEnvironment::new(&mut heap, arguments);
-
-    PreparedProgram {
-        db,
-        byte_code,
-        heap,
-        environment,
-        environment_argument,
-    }
+    (db, source_code)
 }
 
-fn run_program(mut program: PreparedProgram) {
+#[library_benchmark]
+#[bench::examples_fibonacci(vm_runtime_prepare("Examples/fibonacci.candy", &[]))]
+#[bench::examples_hello_world(vm_runtime_prepare("Examples/helloWorld.candy", &["15"]))]
+fn vm_runtime(mut program: PreparedProgram) {
     let vm = Vm::for_main_function(
         program.byte_code,
         &mut program.heap,
@@ -92,14 +67,43 @@ fn run_program(mut program: PreparedProgram) {
     });
 }
 
-// benchmarks = compilation_hello_world, compilation_fibonacci
+struct PreparedProgram {
+    db: Database,
+    byte_code: ByteCode,
+    heap: Heap,
+    environment: BenchmarkingEnvironment,
+    environment_argument: Struct,
+}
+fn vm_runtime_prepare(file_path: &str, arguments: &[&str]) -> PreparedProgram {
+    init_logger();
+
+    let source_code = fs::read_to_string(format!("../../packages/{file_path}")).unwrap();
+
+    let mut db = setup();
+    let byte_code = compile(&mut db, &source_code);
+
+    let mut heap = Heap::default();
+    let (environment_argument, environment) = BenchmarkingEnvironment::new(&mut heap, arguments);
+
+    PreparedProgram {
+        db,
+        byte_code,
+        heap,
+        environment,
+        environment_argument,
+    }
+}
+
 #[allow(unused_mut)]
 library_benchmark_group!(
-    name = compilation_group;
-    benchmarks = compilation_hello_world, compilation_fibonacci
+    name = main;
+    benchmarks = compile, vm_runtime
 );
 #[allow(unused_mut)]
-main!(library_benchmark_groups = compilation_group);
+main!(
+    config = LibraryBenchmarkConfig::default().flamegraph(FlamegraphConfig::default());
+    library_benchmark_groups = main
+);
 
 fn init_logger() {
     let writer = BoxMakeWriter::new(std::io::stderr);
