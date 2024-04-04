4b2530c66beec3c20ce3b946dd854f817525afed
Jonas Wanke <contact@jonas-wanke.com>
2024-04-04 22:14:21 +0200

    Revert "Add fast case for InlineObject's eq"
    This reverts commit 1769bdb25e5fbe7383d3c3befb0357889d681d04.

--- compiler/vm/src/heap/object_inline/mod.rs
+++ compiler/vm/src/heap/object_inline/mod.rs
@@ -119,7 +119,7 @@ impl_debug_display_via_debugdisplay!(InlineObject);
 impl Eq for InlineObject {}
 impl PartialEq for InlineObject {
     fn eq(&self, other: &Self) -> bool {
-        self.raw_word() == other.raw_word() || InlineData::from(*self) == InlineData::from(*other)
+        InlineData::from(*self) == InlineData::from(*other)
     }
 }
 impl Hash for InlineObject {
